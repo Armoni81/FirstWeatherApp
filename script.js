f9abf11b15f8d4011ed528df468c97eb3e30b074
Armoni Tigner <atigner81@gmail.com>
2025-01-30 16:05:40 -0500

    fixed ohio to cleveland

--- script.js
+++ script.js
@@ -10,78 +10,31 @@ let box4 = document.getElementById("box4")
 
 
 
+const arrayOfHomeCities = [ 'Atlanta', 'Cleveland', 'Chicago', 'New York']
 
 
-fetch("https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=7983f9630bd1335bc5529b4c48cf145a")
-    .then(function(response){
-        console.log(response)
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        return data
-    })
-    .then(function(data){
-        console.log(data.main.temp)
-        let temp = data.main.temp
-       var element= document.getElementById("ATL")
-       let i = (temp - 273.15) * 9/5 +32
-       element.innerHTML = i.toFixed(0) + "°"
-       return data
-    })
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=7983f9630bd1335bc5529b4c48cf145a")
-    .then(function(response){
-        console.log(response)
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        return data
-    })
+const func = (arr) => {
+    arr.map( element => {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${element}&appid=7983f9630bd1335bc5529b4c48cf145a`)
+            .then(function(response){
+                console.log(response)
+                return response.json()
+            })
+            .then(function(data){
+                console.log(data)
+                return data
+            })
+            .then(function(data){
+                let temp = data.main.temp
+                const grabCityInitials = element.subString(0,3).toUppercase()
+               var element= document.getElementById(grabCityInitials)
+               let i = (temp - 273.15) * 9/5 +32
+               element.innerHTML = i.toFixed(0) + "°"
+               return data 
+            })
+    } )
+}
 
-    .then(function(data){
-        console.log(data.main.temp)
-        let temp= data.main.temp 
-        var element = document.getElementById("chi") 
-        let i = (temp - 273.15) * 9/5 +32
-        element.innerHTML= i.toFixed(0) + "°"
-
-        return data 
-
-    })
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=7983f9630bd1335bc5529b4c48cf145a")
-    .then(function(response){
-        console.log(response)
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        return data
-    })
-    .then(function(data){
-        console.log(data.main.temp)
-        let temp = data.main.temp
-        let element= document.getElementById("cha")
-        let i = (temp - 273.25) * 9/5 + 32
-        element.innerHTML = i.toFixed(0) + "°"
-    })
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Ohio&appid=7983f9630bd1335bc5529b4c48cf145a")
-    .then(function(response){
-        console.log(response)
-        return response.json()
-    })
-    .then(function(data){
-        console.log(data)
-        return data
-    })
-    .then(function(data){
-        console.log(data.main.temp)
-        let temp = data.main.temp
-        let element= document.getElementById("oh")
-        let i = (temp - 273.25) * 9/5 + 32
-        element.innerHTML = i.toFixed(0) + "F°"  
-    
-    })
 
     function loadBox1 (){
         let cityName = input.value;
