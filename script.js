2e9d71aa6b71ad8c3fe39a7e3e587126bc626bcf
Armoni Tigner <atigner81@gmail.com>
2025-01-30 17:29:18 -0500

    formatting

--- script.js
+++ script.js
@@ -7,47 +7,39 @@ let box4 = document.getElementById("box4");
 const arrOfMajorCities = ["Atlanta", "Chicago", "Charlotte", "Cleveland"];
 
 arrOfMajorCities.forEach( async (city) => {
-  try{
-      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7983f9630bd1335bc5529b4c48cf145a`)
-      const data = await response.json()
-      let element = document.getElementById(city.substring(0, 3));
-      let toFarenheight = ((data.main.temp - 273.15) * 9) / 5 + 32;
-      element.innerHTML = toFarenheight.toFixed(0) + "°";
-  } catch (error){
-    console.error(error)
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7983f9630bd1335bc5529b4c48cf145a`
+    );
+    const data = await response.json();
+    let element = document.getElementById(city.substring(0, 3));
+    let toFarenheight = ((data.main.temp - 273.15) * 9) / 5 + 32;
+    element.innerHTML = toFarenheight.toFixed(0) + "°";
+  } catch (error) {
+    console.error(error);
   }
- 
 });
 
-function loadBox1() {
+const loadWeatherData = async () => {
   let cityName = input.value;
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7983f9630bd1335bc5529b4c48cf145a`
-  )
-    .then(function (response) {
-      console.log(response);
-      return response.json();
-    })
-    .then(function (data) {
-      //console.log(data)
-      return data;
-    })
-    .then(function (data) {
-      console.log(data.main.temp);
-      let temp = data.main.temp;
-      let box1 = document.getElementById("box1");
-      let i = ((temp - 273.25) * 9) / 5 + 32;
-      box1.innerHTML = i.toFixed(0) + " " + "Degrees";
-      forecast.innerHTML = cityName + " " + " Forecast";
-      let windSpeed = data.wind.speed;
-      box2.innerHTML = windSpeed + " " + "Wind/Speed";
-      let humidity = data.main.humidity;
-      box3.innerHTML = humidity + " " + "Humidity";
-      let weather = data.weather[0].description;
-      box4.innerHTML = weather;
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=7983f9630bd1335bc5529b4c48cf145a`
+    );
+    const data = await response.json();
+    console.log(data, 'data')
+    let box1 = document.getElementById("box1");
+    let toFarenheight = ((data.main.temp - 273.25) * 9) / 5 + 32;
+    let weather = data.weather[0].description;
 
-      return data;
-    });
+    box1.innerHTML = toFarenheight.toFixed(0) + " " + "Degrees";
+    forecast.innerHTML = cityName + " " + " Forecast";
+    box2.innerHTML = data.wind.speed + " " + "Wind/Speed";
+    box3.innerHTML = data.main.humidity + " " + "Humidity";
+    box4.innerHTML = weather;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-button.addEventListener("click", loadBox1);
+button.addEventListener("click", loadWeatherData);
